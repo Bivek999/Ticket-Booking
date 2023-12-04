8c975c0b6be33f253ae85a5e35b42f257708d4f1
Bivek999 <118502596+Bivek999@users.noreply.github.com>
2023-12-04 15:02:44 +0530

    Update Status.js

--- src/component/status/Status.js
+++ src/component/status/Status.js
@@ -8,17 +8,17 @@ export default function Status(){
         <div id="status">
             <div className="status-inner-wrapper">
                 <div className="available">
-                    <span><Seatsvg fill='none' width={30} /></span>
+                    <span><Seatsvg fill='none' width={25} /></span>
                     <h2>Available</h2>
                 </div>
 
                 <div className="unavailable">
-                    <span><Seatsvg fill='#808080' width={30} /></span>
+                    <span><Seatsvg fill='#808080' width={25} /></span>
                     <h2>Sold</h2>
                 </div>
                 
                 <div className="selection">
-                    <span><Seatsvg fill='#88C900' width={30} /></span>
+                    <span><Seatsvg fill='#88C900' width={25} /></span>
                     <h2>Your Selection</h2>
                 </div>
             </div>
